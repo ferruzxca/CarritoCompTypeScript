e475f07b6524e12dad4cfd02014ad1991a876a32
ferruzxca <ferruzxca@gmail.com>
2025-09-30 01:38:08 -0600

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,20 @@
-import { defineConfig } from 'vite';
+// vite.config.ts
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
 export default defineConfig({
-  // no pongas base, o base: '/'
+  base: "/",              // Pages usa raíz
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index:   resolve(__dirname, "index.html"),
+        products: resolve(__dirname, "products.html"),
+        cart:     resolve(__dirname, "cart.html"),
+        checkout: resolve(__dirname, "checkout.html"),
+        login:    resolve(__dirname, "login.html"),
+        thanks:   resolve(__dirname, "thanks.html"),
+      },
+    },
+  },
 });
